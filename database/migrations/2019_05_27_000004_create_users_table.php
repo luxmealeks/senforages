e56eab0ce09999bd9532550566d539a80af22630
luxmealeks <luxmealeks@gmail.com>
2019-05-29 07:06:39 +0000

    MAJ utilisateurs

--- database/migrations/2019_05_27_000004_create_users_table.php
+++ database/migrations/2019_05_27_000004_create_users_table.php
@@ -7,37 +7,36 @@ use Illuminate\Database\Migrations\Migration;
 class CreateUsersTable extends Migration
 {
     /**
-     * Schema table name to migrate
+     * Schema table name to migrate.
+     *
      * @var string
      */
     public $tableName = 'users';
 
     /**
      * Run the migrations.
-     * @table users
      *
-     * @return void
+     * @table users
      */
     public function up()
     {
         Schema::create($this->tableName, function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->increments('id');
-            $table->char('uuid', 36);
+            $table->char('uuid', 36)->nullable();
             $table->string('firstname', 200)->nullable();
             $table->string('name', 200)->nullable();
             $table->string('telephone', 200)->nullable();
             $table->string('email', 200)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
-            $table->unsignedInteger('roles_id');
+            $table->unsignedInteger('roles_id')->nullable();
 
-            $table->index(["roles_id"], 'fk_users_roles1_idx');
+            $table->index(['roles_id'], 'fk_users_roles1_idx');
 
-            $table->unique(["email"], 'email_UNIQUE');
+            $table->unique(['email'], 'email_UNIQUE');
             $table->softDeletes();
             $table->nullableTimestamps();
-
 
             $table->foreign('roles_id', 'fk_users_roles1_idx')
                 ->references('id')->on('roles')
@@ -48,11 +47,9 @@ class CreateUsersTable extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-     public function down()
-     {
-       Schema::dropIfExists($this->tableName);
-     }
+    public function down()
+    {
+        Schema::dropIfExists($this->tableName);
+    }
 }
